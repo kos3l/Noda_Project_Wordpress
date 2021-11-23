3a228e8e7c75574f85745b356276ae36c0e5280f
kos3l <71730597+kos3l@users.noreply.github.com>
2021-11-23 13:37:40 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,5 +1,28 @@
 
+    <div class="row">
+        <div class="col-sm"> 
+            <h5>FIRMA</h5> 
+            <p>
+            Noda Byg ApS
+            CVR: 232342342
+            </p>
+        </div>
+        <div class="col-sm">
+        <h5>FIRMA</h5> 
+            <p>
+            Noda Byg ApS
+            CVR: 232342342
+            </p>
+        </div>
+        <div class="col-sm">
+        <h5>FIRMA</h5> 
+            <p>
+            Noda Byg ApS
+            CVR: 232342342
+            </p>
+        </div>
 
+    </div>
 </div>
 <?php wp_footer(); ?>
 </body>
