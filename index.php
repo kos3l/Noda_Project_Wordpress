fed12b38719e54f38f53df128dc727e1b138c2fc
kos3l <71730597+kos3l@users.noreply.github.com>
2021-11-24 12:26:19 +0100

    started index

--- index.php
+++ index.php
@@ -2,12 +2,55 @@
     <div class="row video">
         <div class="col-12"></div>
     </div>
-    <div class="row main-body px-0 mx-0" >
-        <div class="col d-flex justify-content-between px-5 pt-5">
-            <h1>DEAL IS A DEAL</h1>
+    <div class="row px-0 mx-0 main-body" >
+        <div class="col d-flex justify-content-between align-items-center px-5 pt-5">
+            <div> 
+                <h1>DEAL IS A DEAL</h1>
+                <p >We are a trustworthy company that offer Great quality for lower prices  </p>
+            </div>
+            <h5>(+45) 50 50 50 50</h5>
             <button class="contactBtn p-4">
                 <h4 class="px-3 mb-0">CONTACT US</h4>
             </button>
+        </div>
+    </div>
+    <div class="row main-body p-5 mx-0">
+        <div class="col px-0 mx-0">
+            <h4>SERVICES</h4>
+            <p>
+                Noda Byg Aps is a carpentry company that provides mid to high
+                custom carpentry work. We specialize in interior and also exterior
+                work using only the best materials and joinery techniques. 
+
+                Our work includes building custom furniture, windows, doors, 
+                terraces, roofs, flooring and painting.
+            </p>
+        </div>
+        <div class="col">
+            <img src="" alt="">
+        </div>
+    </div>
+    <div class="row main-body p-5 mb-5 mx-0">
+        <div class="col-sm-12 px-0 line-height">
+            <div class="line"></div>
+        </div>
+        <div class="col px-0 py-3  mx-0">
+            <h6 class="mb-0 px-4">ROOFING</h6>
+        </div>
+        <div class="col-sm-12 px-0 line-height">
+            <div class="line"></div>
+        </div>
+        <div class="col px-0 py-3 mx-0">
+            <h6 class="mb-0 px-4">DOORS</h6>
+        </div>
+        <div class="col-sm-12 px-0 line-height">
+            <div class="line"></div>
+        </div>
+        <div class="col px-0 py-3 mx-0">
+            <h6 class="mb-0 px-4">WINDOWS</h6>
+        </div>
+        <div class="col-sm-12 px-0 line-height">
+            <div class="line"></div>
         </div>
 
     </div>
