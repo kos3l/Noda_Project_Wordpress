caf25a1e6ab5442b66adb42b66db0963f97dfb85
kos3l <71730597+kos3l@users.noreply.github.com>
2021-11-25 18:26:36 +0100

    fields

--- about.php
+++ about.php
@@ -57,7 +57,7 @@ Template name: About
             </div>
         </div>
         <div class="col-12 col-lg-6 aboutImg d-flex align-items-center justify-content-center">
-            <img src="<?php the_field('home_logo') ?>" alt="">
+            <img src="<?php the_field('logo_in_about_us') ?>" alt="">
         </div>
     </div>
     <div class="row under main-body mx-0 mb-5 ">
--- index.php
+++ index.php
@@ -13,9 +13,9 @@
         <div class="col d-flex flex-column-reverse flex-row-lg-reverse justify-content-between align-items-center px-md-5 pt-5 flex-lg-row flex-column">
             <div> 
                 <h1>DEAL IS A DEAL</h1>
-                <p >We are a trustworthy company that offer Great quality for lower prices  </p>
+                <p ><?php the_field("under_title")?> </p>
             </div>
-            <h5>(+45) 50 50 50 50</h5>
+            <h5><?php the_field("phone_number")?></h5>
             <button class="contactBtn p-md-4">
                 <h4 class="px-3 mb-0">CONTACT US</h4>
             </button>
@@ -25,17 +25,11 @@
         <div class="col-12 col-lg-6 px-0 mx-0">
             <h4>SERVICES</h4>
             <p>
-                Noda Byg Aps is a carpentry company that provides mid to high
-                custom carpentry work. We specialize in interior and also exterior
-                work using only the best materials and joinery techniques. 
-
-
-                Our work includes building custom furniture, windows, doors, 
-                terraces, roofs, flooring and painting.
+            <?php the_field("service_section_body")?>
             </p>
         </div>
         <div class="col-12 col-lg-6">
-            <img src="" alt="">
+            <img src="<?php the_field('home_logo') ?>" alt="">
         </div>
     </div>
     <div class="row main-body p-md-5 mb-5 mx-0">
@@ -43,19 +37,19 @@
             <div class="line"></div>
         </div>
         <div class="col px-0 py-4  mx-0">
-            <h6 class="mb-0 px-4">ROOFING</h6>
+            <h6 class="mb-0 px-4"><?php the_field("service_1")?></h6>
         </div>
         <div class="col-sm-12 px-0 line-height">
             <div class="line"></div>
         </div>
         <div class="col px-0 py-4 mx-0">
-            <h6 class="mb-0 px-4">DOORS</h6>
+            <h6 class="mb-0 px-4"><?php the_field("service_2")?></h6>
         </div>
         <div class="col-sm-12 px-0 line-height">
             <div class="line"></div>
         </div>
         <div class="col px-0 py-4 mx-0">
-            <h6 class="mb-0 px-4">WINDOWS</h6>
+            <h6 class="mb-0 px-4"><?php the_field("service_3")?></h6>
         </div>
         <div class="col-sm-12 px-0 line-height">
             <div class="line"></div>
@@ -69,15 +63,9 @@
             <img src="" alt="">
         </div>
         <div class="col-12 col-lg-6 px-0 mx-0">
-            <h4>ABOUT US</h4>
+            <h4><?php the_field("under_title")?></h4>
             <p>
-                Noda Byg Aps is a carpentry company that provides mid to high
-                custom carpentry work. We specialize in interior and also exterior
-                work using only the best materials and joinery techniques. 
-
-
-                Our work includes building custom furniture, windows, doors, 
-                terraces, roofs, flooring and painting.
+            <?php the_field("under_title")?>
             </p>
         </div>
     </div>
@@ -96,13 +84,7 @@
     <div class="row main-body location  p-md-5 mx-0 mb-5 ">
         <div class="col-12 col-sm-6 d-flex flex-column justify-content-center ">
             <h3>WHERE TO FIND US</h3>
-            <p>
-                We are closer than you think. You can find our company at:
-                Address
-                Post number
-                Address
-
-                And if you have any further question, send us message or call us.
+            <p><?php the_field("under_title")?>
             </p>
             <button class="locationBtn p-md-4 mt-4">
                 <h4 class="px-3 mb-0">CONTACT US</h4>
