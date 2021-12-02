11ed7c6c8e1e466f1125240a877026562107af0e
kos3l <71730597+kos3l@users.noreply.github.com>
2021-12-02 11:34:31 +0100

    fixed nav

--- header.php
+++ header.php
@@ -12,37 +12,40 @@
 
 
 <nav class="navbar navbar-expand-lg  navbar-dark bg-color fixed-top">
-  <div class="container">
-    <a class="navbar-brand" href="<?php echo get_home_url(); ?>">
-      Noda<br>Byg ApS  <!-- Have to do something like image  -->
-      <?php
-      if(function_exists('the_custom_logo')){
-        $custom_logo_id = get_theme_mod('custom_logo');
-        $logo = wp_get_attachment_image_src($custom_logo_id);
-      } 
-      
-      ?>
-      <img src='<?php echo $logo[0] ?>' alt="">
-      <div class="div-logo-test">
-      <img src="<?php the_field("home_gallery_1");?>" width="60px" alt="">
-      </div>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <?php
-        wp_nav_menu( array(
-            'theme_location'    => 'top-menu',
-            'depth'             => 1,
-            'container'         => 'div',
-            'container_class'   => 'collapse navbar-collapse',
-            'container_id'      => 'navbarSupportedContent',
-            'menu_class'        => 'navbar-nav  ms-auto',
-            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-            'walker'            => new WP_Bootstrap_Navwalker(),
-        ) );
+  <div class="container p-0">
+    <div class="toggleCont px-3">
+      <a class="navbar-brand" href="<?php echo get_home_url(); ?>">
+        Noda<br>Byg ApS  <!-- Have to do something like image  -->
+        <?php
+        if(function_exists('the_custom_logo')){
+          $custom_logo_id = get_theme_mod('custom_logo');
+          $logo = wp_get_attachment_image_src($custom_logo_id);
+        } 
+        
         ?>
+        <img src='<?php echo $logo[0] ?>' alt="">
+        <div class="div-logo-test">
+        <img src="<?php the_field("home_gallery_1");?>" width="60px" alt="">
+        </div>
+      </a>
+      <button class="navbar-toggler pe-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+    
+    <div class="colFix">
+      <?php
+          wp_nav_menu( array(
+              'theme_location'    => 'top-menu',
+              'depth'             => 1,
+              'container'         => 'div',
+              'container_class'   => 'collapse navbar-collapse',
+              'container_id'      => 'navbarSupportedContent',
+              'menu_class'        => 'navbar-nav  ms-auto',
+              'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+              'walker'            => new WP_Bootstrap_Navwalker(),
+          ) );
+          ?>
     </div>
   </div>
 </nav>
